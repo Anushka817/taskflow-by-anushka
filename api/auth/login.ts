004903677a411a6fc369431396e2cc706f761712
Anushka Tomar <anushka2022172@gmail.com>
2026-07-16 22:33:00 +0530

    Update login.ts

--- api/auth/login.ts
+++ api/auth/login.ts
@@ -4,7 +4,12 @@
  */
 
 import { Request, Response } from 'express';
-import { readDB, writeDB, hashPassword, generateToken } from '../_db';
+import {
+  readDB,
+  writeDB,
+  hashPassword,
+  generateToken,
+} from '../_db.js';
 
 export default async function handler(req: Request, res: Response) {
   if (req.method !== 'POST') {
